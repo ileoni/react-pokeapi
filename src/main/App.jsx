9c8a6d59631925d7155b2b977294fb1379e416b9
Igor Leon <ileoni>
2023-11-26 21:08:36 -0300

    adding home page

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 
 import Header from './template/Header'
+import Home from '../pages/Home';
 
 function App() {
   return (
     <>
       <Header/>
+      <Home/>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css';
+
+function Home ()
+{
+    return (
+        <>
+            <section className='section-first'>
+            </section>
+            <section className="section-second spikes bg-yellow-fifth">
+            </section>
+            <section className='section-third'>
+            </section>
+        </>
+    );
+}
+
+export default Home;
