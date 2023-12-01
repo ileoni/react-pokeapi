ae4fe45b72b8832dc70a20cb9677c773310218d3
Igor Leon <ileoni>
2023-12-01 09:23:29 -0300

    updating padding cols

--- src/main/template/Section.jsx
+++ src/main/template/Section.jsx
@@ -4,7 +4,7 @@ function Section({ background, columns = true, children })
 {
     return (
         <section className={`${background}`}>
-            <div className={`wrapper__section ${columns ? "wrapper__section--columns": ""}  mx-[120px]`}>
+            <div className={`wrapper__section ${columns ? "wrapper__section--columns": ""}  mx-[120px] py-16`}>
                 {children}
             </div>
         </section>
