6e1e32cf52807dc022b6cb425c4ee9d794e7472a
Igor Leon <ileoni>
2023-12-12 16:23:54 -0300

    adding Loading component
    and applying to details page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+import "./Loading.css";
+
+function Loading()
+{
+    return <div className="loading"></div>
+}
+
+export default Loading;
--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -7,19 +7,28 @@ import Features from './Features';
 import Stats from './Stats';
 import Evolutions from './Evolutions';
 
+import Loading from '../../components/Loading';
+
 function Details()
 {   
+    const [loading, isLoading] = useState(false);
     const [pokemon, setPokemon] = useState(null);
 
     const endpoint = location.pathname;
+
     useEffect(() => {
-        getPokemonDetails(endpoint).then(setPokemon);
+        getPokemonDetails(endpoint)
+            .then(setPokemon)
+            .then(_ => isLoading(true));
     }, [])
 
     return (
         <>
         {
-            pokemon && (
+            !loading && <Loading></Loading>
+        }
+        {
+            loading && (
                 <>
                     <Introduction pokemon={pokemon}/>
                     <Features pokemon={pokemon} />
